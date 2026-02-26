24f960874549b42c9b1a706d7c911e8334e6fbf2
Marcus Vinicius <marcusv892@gmail.com>
2026-02-25 23:43:39 -0300

    style: remove border sidebarapp component

--- app/_components/SidebarApp.tsx
+++ app/_components/SidebarApp.tsx
@@ -18,54 +18,50 @@ import {
 
 export default function SidebarApp() {
   return (
-    <div>
-      <Sidebar className="text-white-100 bg-blue-200">
-        <SidebarHeader className="py-8 text-center text-3xl font-bold">
-          IrisApp
-        </SidebarHeader>
-        <SidebarContent className="ml-1 text-gray-100">
-          <SidebarMenuItem>
-            <SidebarButton href="/">
-              <LayoutDashboard />
-              Dashboard
-            </SidebarButton>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <SidebarButton href="/collaborators">
-              <Users />
-              Colaboradores
-            </SidebarButton>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <SidebarButton href="/themes">
-              <ClipboardList />
-              Temas
-            </SidebarButton>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <SidebarButton href="/sectors">
-              <Building />
-              Setores
-            </SidebarButton>
-          </SidebarMenuItem>
-        </SidebarContent>
-        <SidebarFooter className="flex flex-row items-center justify-between bg-white-100">
-          <div className="flex items-center gap-2">
-            <Avatar className=" bg-gray-100">
-              <AvatarFallback>MV</AvatarFallback>
-            </Avatar>
-            <div>
-              <p className="text-blue-200 text-sm">Marcus Vinícius</p>
-              <span className="text-gray-100 text-xs">
-                Analista de Sistemas
-              </span>
-            </div>
+    <Sidebar className="text-white-100 bg-blue-200 z-50 border-0">
+      <SidebarHeader className="py-8 text-center text-3xl font-bold">
+        IrisApp
+      </SidebarHeader>
+      <SidebarContent className="ml-1 text-gray-100">
+        <SidebarMenuItem>
+          <SidebarButton href="/">
+            <LayoutDashboard />
+            Dashboard
+          </SidebarButton>
+        </SidebarMenuItem>
+        <SidebarMenuItem>
+          <SidebarButton href="/collaborators">
+            <Users />
+            Colaboradores
+          </SidebarButton>
+        </SidebarMenuItem>
+        <SidebarMenuItem>
+          <SidebarButton href="/themes">
+            <ClipboardList />
+            Temas
+          </SidebarButton>
+        </SidebarMenuItem>
+        <SidebarMenuItem>
+          <SidebarButton href="/sectors">
+            <Building />
+            Setores
+          </SidebarButton>
+        </SidebarMenuItem>
+      </SidebarContent>
+      <SidebarFooter className="flex flex-row items-center justify-between bg-white-100">
+        <div className="flex items-center gap-2">
+          <Avatar className=" bg-gray-100">
+            <AvatarFallback>MV</AvatarFallback>
+          </Avatar>
+          <div>
+            <p className="text-blue-200 text-sm">Marcus Vinícius</p>
+            <span className="text-gray-100 text-xs">Analista de Sistemas</span>
           </div>
-          <Button title="Sair">
-            <LogOut className="text-blue-200" />
-          </Button>
-        </SidebarFooter>
-      </Sidebar>
-    </div>
+        </div>
+        <Button title="Sair">
+          <LogOut className="text-blue-200" />
+        </Button>
+      </SidebarFooter>
+    </Sidebar>
   );
 }
