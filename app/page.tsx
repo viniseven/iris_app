d4d2afa9044223b226dd471a8711e9c2062fac8d
Marcus Vinicius <marcusv892@gmail.com>
2026-02-25 23:43:08 -0300

    remove: component header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Header from "./_components/Header";
 import PageTitle from "./_components/PageTitle";
 
 export default function DashboardPage() {
   return (
     <div className="w-full">
-      <Header />
       <PageTitle title="Dashboard" description="Visão geral dos indicadores" />
     </div>
   );
--- app/sectors/page.tsx
+++ app/sectors/page.tsx
@@ -1,5 +1,4 @@
 import { Plus } from "lucide-react";
-import Header from "../_components/Header";
 import PageContent from "../_components/PageContent";
 import PageTitle from "../_components/PageTitle";
 import TitlePageButton from "../_components/TitlePageButton";
@@ -7,7 +6,6 @@ import TitlePageButton from "../_components/TitlePageButton";
 export default function SectorsPage() {
   return (
     <div className="w-full">
-      <Header />
       <PageContent>
         <PageTitle
           title="Setores"
