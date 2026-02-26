3204a7fe0a0b8d7d224c0b310cb15642e015a178
Marcus Vinicius <marcusv892@gmail.com>
2026-02-25 22:34:17 -0300

    feat: add pageContent component for components page

--- app/collaborators/page.tsx
+++ app/collaborators/page.tsx
@@ -1,14 +1,23 @@
+import { Plus } from "lucide-react";
 import Header from "../_components/Header";
+import PageContent from "../_components/PageContent";
 import PageTitle from "../_components/PageTitle";
+import TitlePageButton from "../_components/TitlePageButton";
 
 export default function CollaboratorsPage() {
   return (
     <div className="w-full">
       <Header />
-      <PageTitle
-        title="Colaboradores"
-        description="Gerencie colaboradores e facilitadores"
-      />
+      <PageContent>
+        <PageTitle
+          title="Colaboradores"
+          description="Gerencie colaboradores e facilitadores"
+        />
+        <TitlePageButton
+          iconButton={Plus}
+          titleButton="Adicionar colaborador"
+        />
+      </PageContent>
     </div>
   );
 }
--- app/sectors/page.tsx
+++ app/sectors/page.tsx
@@ -1,14 +1,20 @@
+import { Plus } from "lucide-react";
 import Header from "../_components/Header";
+import PageContent from "../_components/PageContent";
 import PageTitle from "../_components/PageTitle";
+import TitlePageButton from "../_components/TitlePageButton";
 
 export default function SectorsPage() {
   return (
     <div className="w-full">
       <Header />
-      <PageTitle
-        title="Setores"
-        description="Gerencie os setores cadastrados"
-      />
+      <PageContent>
+        <PageTitle
+          title="Setores"
+          description="Gerencie os setores cadastrados"
+        />
+        <TitlePageButton iconButton={Plus} titleButton="Adicionar setor" />
+      </PageContent>
     </div>
   );
 }
