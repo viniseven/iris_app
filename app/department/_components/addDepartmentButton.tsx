d88f833b048f402816aae02fc3cb1568094cbf3c
Marcus Vinicius <marcusv892@gmail.com>
2026-02-26 23:36:15 -0300

    chore: create component for use client

---
+++ app/department/_components/addDepartmentButton.tsx
@@ -0,0 +1,26 @@
+import TitlePageButton from "@/app/_components/TitlePageButton";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/app/_components/ui/dialog";
+import { Plus } from "lucide-react";
+
+export default function AddDepartmentButton() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <TitlePageButton iconButton={Plus} titleButton="Novo setor" />
+      </DialogTrigger>
+      <DialogContent className="bg-form text-text border-none!">
+        <DialogHeader>
+          <DialogTitle>Cadastrar setor</DialogTitle>
+          <DialogDescription>Insira as informações abaixo</DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
