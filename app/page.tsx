2043fa7a183cb6579f00c8099fa859e0f1b6ec5c
Marcus Vinicius <marcusv892@gmail.com>
2026-02-26 21:33:11 -0300

    delete: pagecontent component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import PageContent from "./_components/PageContent";
 import PageTitle from "./_components/PageTitle";
 
 export default function DashboardPage() {
   return (
     <div className="w-full">
-      <PageTitle title="Dashboard" description="Visão geral dos indicadores" />
+      <PageContent>
+        <PageTitle
+          title="Dashboard"
+          description="Visão geral dos indicadores"
+        />
+      </PageContent>
     </div>
   );
 }
