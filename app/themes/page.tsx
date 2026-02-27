e686574c4e6e89e64d862c3d880978ad0131b671
Marcus Vinicius <marcusv892@gmail.com>
2026-02-26 21:32:08 -0300

    chore: remove page content component and adjust responsive design

--- app/themes/page.tsx
+++ app/themes/page.tsx
@@ -1,15 +1,12 @@
 import { Plus } from "lucide-react";
-import PageContent from "../_components/PageContent";
 import PageTitle from "../_components/PageTitle";
 import TitlePageButton from "../_components/TitlePageButton";
 
 export default function ThemesPage() {
   return (
-    <div className="w-full">
-      <PageContent>
-        <PageTitle title="Temas" description="Gerencie temas de treinamento" />
-        <TitlePageButton iconButton={Plus} titleButton="Adicionar tema" />
-      </PageContent>
+    <div className="flex flex-col mb-6 p-4 w-full gap-2 tablet:flex-row tablet:justify-between tablet:gap-0">
+      <PageTitle title="Temas" description="Gerencie temas de treinamento" />
+      <TitlePageButton iconButton={Plus} titleButton="Adicionar tema" />
     </div>
   );
 }
