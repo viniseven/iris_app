7e73740914b98bdd974c5ab78a0cc455adb5dcac
Marcus Vinicius <marcusv892@gmail.com>
2026-02-26 23:37:25 -0300

    chore: change titlebutton props titlebuttoncomponent

--- app/themes/page.tsx
+++ app/themes/page.tsx
@@ -6,7 +6,7 @@ export default function ThemesPage() {
   return (
     <div className="flex flex-col mb-6 p-4 w-full gap-2 tablet:flex-row tablet:justify-between tablet:gap-0">
       <PageTitle title="Temas" description="Gerencie temas de treinamento" />
-      <TitlePageButton iconButton={Plus} titleButton="Adicionar tema" />
+      <TitlePageButton iconButton={Plus} titleButton="Novo tema" />
     </div>
   );
 }
