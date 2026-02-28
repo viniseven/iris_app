7740c4f0e998f2c4b162220cef00662118203320
Marcus Vinicius <marcusv892@gmail.com>
2026-02-28 12:54:09 -0300

    chore: name component and submit button

---
+++ app/sectors/_components/registerSectorDialog.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import {
+  default as ButtonComponent,
+  default as HeaderButton,
+} from "@/app/_components/ButtonComponent";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/app/_components/ui/dialog";
+import { Field, FieldLabel } from "@/app/_components/ui/field";
+import { Input } from "@/app/_components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Plus } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
+import * as z from "zod";
+
+const formSectorSchema = z.object({
+  name: z.string().trim().min(1, { error: "Nome é obrigatório" }),
+});
+
+type FormSchema = z.infer<typeof formSectorSchema>;
+
+export default function RegisterSectorDialog() {
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSectorSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+  function onSubmit(data: FormSchema) {
+    console.log(data);
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <HeaderButton iconButton={Plus} titleButton="Novo setor" />
+      </DialogTrigger>
+      <DialogContent className="bg-form text-text border-none!">
+        <DialogHeader>
+          <DialogTitle>Cadastrar setor</DialogTitle>
+          <DialogDescription className="text-gray-100">
+            Insira as informações abaixo
+          </DialogDescription>
+        </DialogHeader>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <Controller
+            name="name"
+            control={form.control}
+            render={({ field }) => (
+              <Field>
+                <FieldLabel htmlFor={field.name}>Nome</FieldLabel>
+                <Input
+                  {...field}
+                  id={field.name}
+                  placeholder="Insira o nome do setor"
+                  autoComplete="off"
+                />
+              </Field>
+            )}
+          />
+          <DialogFooter className="mt-6">
+            <ButtonComponent type="submit" titleButton="Cadastrar" />
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
