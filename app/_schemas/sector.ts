b48113c6d9dd7dd84a85a4b576fe5ee0f4c1d922
Marcus Vinicius <marcusv892@gmail.com>
2026-03-01 02:41:21 -0300

    chore: regex update for underline accept

--- app/_schemas/sector.ts
+++ app/_schemas/sector.ts
@@ -4,7 +4,7 @@ export const formSectorSchema = z.object({
   name: z
     .string()
     .min(1, "O nome é obrigatório")
-    .regex(/^[a-zA-Z\s]+$/, "Apenas letras são permitidos e sem acentos")
+    .regex(/^[a-zA-Z\s_]+$/, "Apenas letras são permitidos e sem acentos")
     .toLowerCase()
     .transform((val) => val.replace(/\s+/g, "_")),
 });
