e316f0547cb0b5f752a40b61b4b97df8e909715a
Marcus Vinicius <marcusv892@gmail.com>
2026-03-01 02:41:34 -0300

    chore: add card component

--- app/sectors/page.tsx
+++ app/sectors/page.tsx
@@ -1,14 +1,31 @@
 import PageTitle from "@/app/_components/PageTitle";
+import CardComponent from "../_components/CardComponent";
+import { getAllSectors } from "../_data_access/sector";
 import RegisterSectorDialog from "./_components/registerSectorDialog";
 
-export default function SectorsPage() {
+export default async function SectorsPage() {
+  const resultSectors = await getAllSectors();
+
+  console.log(resultSectors);
+
   return (
-    <div className="flex flex-col mb-6 p-4 w-full gap-2 tablet:flex-row tablet:justify-between tablet:gap-0">
-      <PageTitle
-        title="Setores"
-        description="Gerencie os setores cadastrados"
-      />
-      <RegisterSectorDialog />
+    <div className="w-full p-4 mb-6 flex flex-col gap-4">
+      <div className="flex flex-col   w-full gap-2 tablet:flex-row tablet:justify-between tablet:gap-0">
+        <PageTitle
+          title="Setores"
+          description="Gerencie os setores cadastrados"
+        />
+        <RegisterSectorDialog />
+      </div>
+
+      <div className="grid grid-cols-1 gap-6 tablet:grid-cols-2 laptop:grid-cols-3">
+        {resultSectors.map((sector) => (
+          <CardComponent
+            key={sector.id}
+            title={sector.name.replaceAll("_", " ").toUpperCase()}
+          />
+        ))}
+      </div>
     </div>
   );
 }
