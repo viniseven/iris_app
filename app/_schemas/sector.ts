17cbdfd9058e93ad37f1e4dc432ccb9cde7fe6d2
Marcus Vinicius <marcusv892@gmail.com>
2026-03-01 00:48:32 -0300

    refactor: schema formsector

---
+++ app/_schemas/sector.ts
@@ -0,0 +1,12 @@
+import * as z from "zod";
+
+export const formSectorSchema = z.object({
+  name: z
+    .string()
+    .min(1, "O nome é obrigatório")
+    .regex(/^[a-zA-Z\s]+$/, "Apenas letras são permitidos e sem acentos")
+    .toLowerCase()
+    .transform((val) => val.replace(/\s+/g, "_")),
+});
+
+export type FormSchema = z.infer<typeof formSectorSchema>;
