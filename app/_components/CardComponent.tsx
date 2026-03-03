90e4491a1948b1ab3b06ad628e9ee885777cc7e8
Marcus Vinicius <marcusv892@gmail.com>
2026-03-03 00:38:19 -0300

    chore: add card component

--- app/_components/CardComponent.tsx
+++ app/_components/CardComponent.tsx
@@ -1,20 +1,57 @@
-import { Building } from "lucide-react";
-import { Card, CardHeader, CardTitle } from "./ui/card";
+"use client";
+
+import { Building, EllipsisVertical, Pencil } from "lucide-react";
+import DeleteSectorDialog from "../sectors/_components/deleteSectorDialog";
+import { Button } from "./ui/button";
+import { Card, CardAction, CardHeader, CardTitle } from "./ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 interface CardComponentProps {
   title: string;
+  id: string;
 }
 
-export default function CardComponent({ title }: CardComponentProps) {
+export default function CardComponent({ title, id }: CardComponentProps) {
   return (
-    <Card className="bg-card border-0">
-      <CardHeader className="flex gap-6">
-        <Building
-          className="bg-white-200 text-gray-100 p-1 rounded-lg"
-          size={60}
-        />
-        <CardTitle>{title}</CardTitle>
-      </CardHeader>
+    <Card className="bg-card border-0 flex py-3">
+      <div className="flex ml-2 gap-4">
+        <div className="border-r border-white-200">
+          <Building className="text-gray-100 p-1 rounded-lg" size={60} />
+        </div>
+
+        <CardHeader className="flex justify-between  w-full h-full content-start p-0">
+          <CardTitle>{title}</CardTitle>
+          <CardAction>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  className="border-none shadow-none p-0"
+                >
+                  <EllipsisVertical className="text-button-options" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="border-none bg-card text-button-options">
+                <DropdownMenuItem className="p-2">
+                  <Pencil />
+                  Editar
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onSelect={(e) => e.preventDefault()}
+                  className="p-0"
+                >
+                  <DeleteSectorDialog id={id} />
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </CardAction>
+        </CardHeader>
+      </div>
     </Card>
   );
 }
